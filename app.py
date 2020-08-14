a31be51127d13942a3992dd2ba4c79dea80b14ea
Anas Alaqeel <anasalaqeel@hotmail.com>
2020-08-14 20:03:08 +0300

    add movies patch request

--- app.py
+++ app.py
@@ -10,10 +10,16 @@ setup_db(app)
 # movies
 #------------------------------------------------------------
 @app.route("/movies")
-def index():
+def movies():
     get_movies = Movies.query.all()
-    print(get_movies)
-    return 'Hello World'
+    movies = []
+    for movie in get_movies:
+      movies.append({
+        "title": movie.title,
+        "releaseDate": movie.release_date
+      })
+
+    return jsonify({"movies": movies})
 
 @app.route("/movies/create", methods=["POST"])
 def create_movie():
@@ -34,6 +40,29 @@ def create_movie():
             abort (422)
         else:
             return jsonify({'success': True})
+
+@app.route("/movies/<movie_id>", methods=["PATCH"])
+def movie_edit(movie_id):
+  error = False
+  get_movie = Movies.query.filter_by(id=movie_id).first
+  title = request.get_json()['title']
+  release_date = request.get_json()['releaseDate']
+  try:
+        get_movie.title=title
+        get_movie.release_date=release_date
+        get_movie.update()
+  except:
+      erroe = True
+      print(sys.exc_info())
+  finally:
+      if error:
+          abort (422)
+      else:
+          return jsonify({'success': True, "id": get_movie.id})
+
+# actors
+#------------------------------------------------------------
+
 
 
 if __name__ == '__main__':
